a481538dd1d6890e48ac5ea2824c6db9f7582033
c0gnater <87967876+c0gnater@users.noreply.github.com>
2021-08-11 14:51:10 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var box2;
+var box1;
+
+function setup() {
+  createCanvas(600, 400);
+  box1 = new Box();
+  box2 = new Box();
+}
+
+function draw() {
+  background(220);
+box1.show();
+box2.show();
+box1.set_width(80);
+}
+
